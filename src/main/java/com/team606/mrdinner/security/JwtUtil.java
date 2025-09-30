01195b0396450181593f8d4c5f6c6c15ee2bb9f7
Hwang Soojin <soojin9433@naver.com>
2025-10-01 01:39:45 +0900

    JwtUtil.java와 JwtFilter.java 파일을 추가

---
+++ src/main/java/com/team606/mrdinner/security/JwtUtil.java
@@ -0,0 +1,36 @@
+package com.team606.mrdinner.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long EXPIRATION = 1000 * 60 * 60; // 1시간
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(key)
+                .compact();
+    }
+
+    public String validateAndGetUsername(String token) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+        } catch (JwtException e) {
+            return null;
+        }
+    }
+}
