b22130c3408d416dc9e012f9e4ca008998b8a058
Hwang Soojin <soojin9433@naver.com>
2025-11-14 00:51:57 +0900

    feat(order): cartedTime 기반 주문 조회 기능 추가

--- backend/src/main/java/com/team606/mrdinner/repository/OrderRepository.java
+++ backend/src/main/java/com/team606/mrdinner/repository/OrderRepository.java
@@ -1,5 +1,15 @@
 package com.team606.mrdinner.repository;
 
 import com.team606.mrdinner.entity.Order;
+import com.team606.mrdinner.entity.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface OrderRepository extends JpaRepository<Order, Long> {}
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByCustomerUsernameAndStatus(String username, OrderStatus status);
+
+    Optional<Order> findByCustomerUsernameAndCartedTime(String username, Instant cartedTime);
+}
