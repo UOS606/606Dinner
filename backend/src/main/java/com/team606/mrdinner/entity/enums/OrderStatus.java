78465150bedeaf2fb939ce1774ed85874fcc1e7f
Hwang Soojin <soojin9433@naver.com>
2025-11-14 00:53:22 +0900

    feat(order): 주문 상태 enum에 ORDERED 추가

--- backend/src/main/java/com/team606/mrdinner/entity/enums/OrderStatus.java
+++ backend/src/main/java/com/team606/mrdinner/entity/enums/OrderStatus.java
@@ -1,2 +1,2 @@
 package com.team606.mrdinner.entity.enums;
-public enum OrderStatus { CARTED, RECEIVED, COOKED, DELIVERED, CANCELLED }
+public enum OrderStatus { CARTED, ORDERED, RECEIVED, COOKED, DELIVERED, CANCELLED }
